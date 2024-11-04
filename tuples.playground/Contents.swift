1908bc7619a6d84aa7100fe8ef9eb84ef1778f99
Okan Aktaş <okanaktas02@gmail.com>
2024-11-05 02:11:48 +0300

    Tuples

--- tuples.playground/Contents.swift
+++ tuples.playground/Contents.swift
@@ -5,3 +5,16 @@ var ogrenci : (Int,(Bool,String)) = (1996,(true,"Okan"))
 var yil = ogrenci.0
 var siniftaMi = ogrenci.1.0
 var isim = ogrenci.1.1
+
+
+var nokta = (x:10,y:20)
+
+print(nokta.x)
+print(nokta.y)
+
+var hataMesajı = (404, "Hata Mesajı")
+
+var (kod, mesaj) = hataMesajı
+
+print(kod)
+print(mesaj)

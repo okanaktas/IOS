3ad3fa693506b2132af980271a6369e8bf8d800a
Okan Aktaş <okanaktas02@gmail.com>
2024-11-07 03:48:26 +0300

    Recap

--- recap.playground/Contents.swift
+++ recap.playground/Contents.swift
@@ -37,3 +37,38 @@ for i in dizi{
 for(index, dizi) in dizi.enumerated(){
     print("Sonuc: \(index) - \(dizi)")
 }
+
+var sayilar = [1,3,6,7,9,2,4,5,8]
+
+print("7 den büyük sayılar: \(sayilar.filter{$0 > 7})")
+
+print("2 ile 6 arasındaki sayılar: \(sayilar.filter({$0 >= 2 && $0 <= 6}))")
+
+let notlar = [10,20,30,40,50,60,70,80,90]
+
+var toplamNot = 0
+
+for i in notlar{
+    toplamNot = toplamNot + i
+}
+
+print("Toplam Not: \(toplamNot)")
+print("Ortalama: \(toplamNot/notlar.count)")
+
+let genelDizi = [1,2,3,4,5,6,7,8,9]
+var tekSayilar = [Int]()
+var ciftSSayilar = [Int]()
+
+for i in genelDizi{
+    let sonuc = i % 2
+    
+    if(sonuc == 0){
+        ciftSSayilar.append(i)
+    }
+    if(sonuc == 1){
+        tekSayilar.append(i)
+    }
+}
+
+print("Tek sayılar: \(tekSayilar)")
+print("Çift sayılar: \(ciftSSayilar)")

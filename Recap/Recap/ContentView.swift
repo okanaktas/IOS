0cb03a5756065d163cb375eb73236c043896012e
Okan Aktaş <okanaktas02@gmail.com>
2024-11-09 00:44:27 +0300

    feature

---
+++ Recap/Recap/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Recap
+//
+//  Created by Okan Aktas on 8.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button("Deneme"){
+                print("Tıklandı")
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Recap/Recap/RecapApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecapApp.swift
+//  Recap
+//
+//  Created by Okan Aktas on 8.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct RecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

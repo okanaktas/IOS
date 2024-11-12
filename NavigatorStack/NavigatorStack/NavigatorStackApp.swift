1e5711365778e5847d6ea6d810025907d89e89aa
Okan Aktaş <okanaktas02@gmail.com>
2024-11-13 00:59:09 +0300

    NavigatorStack

---
+++ NavigatorStack/NavigatorStack/NavigatorStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigatorStackApp.swift
+//  NavigatorStack
+//
+//  Created by Okan Aktas on 13.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigatorStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NavigatorStack/NavigatorStack/SecondPage.swift
@@ -0,0 +1,20 @@
+//
+//  SecondPage.swift
+//  NavigatorStack
+//
+//  Created by Okan Aktas on 13.11.2024.
+//
+
+import SwiftUI
+
+struct SecondPage: View {
+    var body: some View {
+        VStack{
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }//.navigationBarBackButtonHidden(true)
+    }
+}
+
+#Preview {
+    SecondPage()
+}

3050f9fa57450bda6870b8e91d09f92f6bb5d1af
Okan Aktaş <okanaktas02@gmail.com>
2024-11-13 06:55:30 +0300

    Toggle And Switch

---
+++ Toggle_Switch/Toggle_Switch/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Toggle_Switch
+//
+//  Created by Okan Aktas on 13.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var switchStatus : Bool = false
+    @State private var switchStatus2 : Bool = false
+    
+    var body: some View {
+        NavigationStack{
+            VStack{
+                Toggle("Switch",isOn: $switchStatus)
+                    .onChange(of: switchStatus){
+                        value in
+                        print("Switch: \(value)")
+                    }
+                    .padding()
+                    .toggleStyle(SwitchToggleStyle(tint: .red))
+                
+                
+                Toggle("Switch",isOn: $switchStatus2).onChange(of: switchStatus){
+                    value in
+                    print("Switch: \(value)")
+                }
+                .padding()
+                
+                Button("Son Durum Print Ekraninda"){
+                    print("Switch: \(switchStatus)")
+                    print("Switch 2: \(switchStatus2)")
+                }
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Toggle_Switch/Toggle_Switch/Toggle_SwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Toggle_SwitchApp.swift
+//  Toggle_Switch
+//
+//  Created by Okan Aktas on 13.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Toggle_SwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

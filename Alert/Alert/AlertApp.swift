b79d269013cab625e65e6c17711d44f801feb465
Okan Aktaş <okanaktas02@gmail.com>
2024-11-16 00:49:00 +0300

    Alert

---
+++ Alert/Alert/AlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertApp.swift
+//  Alert
+//
+//  Created by Okan Aktas on 16.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct AlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Alert/Alert/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Alert
+//
+//  Created by Okan Aktas on 16.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var alertButton = false
+    var body: some View {
+        VStack {
+            Button("Alert"){
+                alertButton = true
+            }
+            .padding()
+            .frame(width: 180,height: 60)
+            .background(.tint)
+            .foregroundStyle(.white)
+            .cornerRadius(8)
+            .alert("Başlık",isPresented: $alertButton) {
+                Button("İptal",role: .cancel){}
+                Button("Tamam",role: .destructive){}
+            }message:{
+                Text("İçerik")
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}

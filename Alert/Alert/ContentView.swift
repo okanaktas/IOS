6429b74eaf01a009a676e7b539b545036b496e3d
Okan Aktaş <okanaktas02@gmail.com>
2024-11-16 00:52:56 +0300

    Alert

--- Alert/Alert/ContentView.swift
+++ Alert/Alert/ContentView.swift
@@ -11,20 +11,33 @@ struct ContentView: View {
     @State private var alertButton = false
     var body: some View {
         VStack {
-            Button("Alert"){
+            
+            
+            Button("Alert Button"){
                 alertButton = true
-            }
-            .padding()
-            .frame(width: 180,height: 60)
-            .background(.tint)
-            .foregroundStyle(.white)
-            .cornerRadius(8)
-            .alert("Başlık",isPresented: $alertButton) {
-                Button("İptal",role: .cancel){}
-                Button("Tamam",role: .destructive){}
-            }message:{
-                Text("İçerik")
-            }
+            }.padding()
+                .frame(width: 180,height: 60)
+                .background(.tint)
+                .foregroundStyle(.white)
+                .cornerRadius(8)
+                .alert("Başlık",isPresented: $alertButton) {
+                    Button("İptal",role: .cancel){
+                        print("İptal")
+                    }
+                    Button("Tamam",role: .destructive){
+                        print("Tamam")
+                    }
+                }message:{
+                    Text("İçerik")
+                }
+            
+            
+            
+            
+            
+            
+            
+            
         }
     }
 }

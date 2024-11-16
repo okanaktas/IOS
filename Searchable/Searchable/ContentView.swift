bfb09f9190b8c3d6e72a7a9620aa8094e93f037a
Okan Aktaş <okanaktas02@gmail.com>
2024-11-16 03:31:36 +0300

    Searchable

---
+++ Searchable/Searchable/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Searchable
+//
+//  Created by Okan Aktas on 16.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var searchedWord: String = ""
+    var body: some View {
+        NavigationStack {
+            VStack {
+                
+            }
+            .navigationTitle("Search")
+            .padding()
+        }.searchable(text: $searchedWord, prompt: "Search")
+            .onChange(of: searchedWord) { s in
+                print("Result for search: \(s)")
+            }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Searchable/Searchable/SearchableApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchableApp.swift
+//  Searchable
+//
+//  Created by Okan Aktas on 16.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SearchableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

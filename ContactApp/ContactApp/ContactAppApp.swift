c6143c329546a03a06072352ef20e7ca74d78f8c
Okan Aktaş <okanaktas02@gmail.com>
2024-11-19 22:38:05 +0300

    ContactApp
    created

---
+++ ContactApp/ContactApp/ContactAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactAppApp.swift
+//  ContactApp
+//
+//  Created by Okan Aktas on 19.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct ContactAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ ContactApp/ContactApp/HomeView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ContactApp
+//
+//  Created by Okan Aktas on 19.11.2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    HomeView()
+}

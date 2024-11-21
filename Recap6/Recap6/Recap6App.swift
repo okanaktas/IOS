1806b92b8736fb51837a9787afbf97034187cb55
Okan Aktaş <okanaktas02@gmail.com>
2024-11-22 02:08:49 +0300

    Recap6

---
+++ Recap6/Recap6/Recap6App.swift
@@ -0,0 +1,17 @@
+//
+//  Recap6App.swift
+//  Recap6
+//
+//  Created by Okan Aktas on 21.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Recap6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

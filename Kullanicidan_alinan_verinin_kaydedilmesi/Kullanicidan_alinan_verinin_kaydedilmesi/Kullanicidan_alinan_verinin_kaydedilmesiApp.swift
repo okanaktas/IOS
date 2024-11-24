973501dcd287536b3e6c4b9e1b49d8be79f99a53
Okan Aktaş <okanaktas02@gmail.com>
2024-11-25 01:30:52 +0300

    Kullanıcıdan alınan verinin kaydedilmesi

---
+++ Kullanicidan_alinan_verinin_kaydedilmesi/Kullanicidan_alinan_verinin_kaydedilmesi/Kullanicidan_alinan_verinin_kaydedilmesiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kullanicidan_alinan_verinin_kaydedilmesiApp.swift
+//  Kullanicidan_alinan_verinin_kaydedilmesi
+//
+//  Created by Okan Aktas on 25.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Kullanicidan_alinan_verinin_kaydedilmesiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

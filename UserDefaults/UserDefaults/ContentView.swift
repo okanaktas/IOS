2026e9b34597852ff49afd77b9f3c455bb98575f
Okan Aktaş <okanaktas02@gmail.com>
2024-11-25 01:48:00 +0300

    User Defaults

---
+++ UserDefaults/UserDefaults/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  UserDefaults
+//
+//  Created by Okan Aktas on 25.11.2024.
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var inputValue = "" // TextField'ın içeriği
+    @State private var lastValue = ""  // Text etiketi için saklanan değer
+    
+    var body: some View {
+        VStack{
+            // Text, sadece butona tıklanınca güncellenir
+            Text("Last Value: \(lastValue)")
+                .onAppear() {
+                    // Kullanıcıdan kaydedilen değeri al
+                    let ud = UserDefaults.standard
+                    //lastValue = ud.string(forKey: "lastValue") ?? "No Value"
+                    if let savedValue = ud.string(forKey: "lastValue"){
+                        lastValue = savedValue
+                    }
+                }
+    
+            // TextField, kullanıcıya giriş yapması için
+            TextField("Enter a value", text: $inputValue)
+                .padding()
+                .textFieldStyle(PlainTextFieldStyle())
+            
+            // Save butonu
+            Button("Save") {
+                // Butona tıklanınca TextField'daki değeri kaydet
+                let ud = UserDefaults.standard
+                ud.set(inputValue, forKey: "lastValue")
+                
+                // TextField'dan değeri al ve Text'e aktar
+                lastValue = inputValue
+                
+                // TextField'ı temizle
+                inputValue = ""
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UserDefaults/UserDefaults/UserDefaultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsApp.swift
+//  UserDefaults
+//
+//  Created by Okan Aktas on 25.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

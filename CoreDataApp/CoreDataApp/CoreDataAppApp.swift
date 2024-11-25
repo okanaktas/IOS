7ae7c5de073aa5f9049036411b6245daa5345504
Okan Aktaş <okanaktas02@gmail.com>
2024-11-26 02:21:16 +0300

    Created
    CoreData App

---
+++ CoreDataApp/CoreDataApp/CoreDataAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataAppApp.swift
+//  CoreDataApp
+//
+//  Created by Okan Aktas on 26.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

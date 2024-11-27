fd44671d094c60368fafd52e71f1b3767984e4da
Okan Aktaş <okanaktas02@gmail.com>
2024-11-27 03:39:02 +0300

    Gorunumler

---
+++ Gorunumler/Gorunumler/GorunumlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GorunumlerApp.swift
+//  Gorunumler
+//
+//  Created by Okan Aktas on 27.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct GorunumlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeScreenView()
+        }
+    }
+}

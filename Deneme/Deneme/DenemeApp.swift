5d150bc759ccae931cd9a55ba1e599df810c6485
Okan Aktaş <okanaktas02@gmail.com>
2024-12-11 04:41:32 +0300

    Deneme

---
+++ Deneme/Deneme/DenemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DenemeApp.swift
+//  Deneme
+//
+//  Created by Okan Aktas on 11.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct DenemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

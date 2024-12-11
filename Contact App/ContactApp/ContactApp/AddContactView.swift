b08d1ad08bdf073bceab06c74886a8447a30859c
Okan Aktaş <okanaktas02@gmail.com>
2024-12-11 04:43:06 +0300

    Created ContactApp

---
+++ Contact App/ContactApp/ContactApp/AddContactView.swift
@@ -0,0 +1,22 @@
+//
+//  AddContactView.swift
+//  ContactApp
+//
+//  Created by Okan Aktas on 11.12.2024.
+//
+
+import SwiftUI
+
+struct AddContactView: View {
+    var body: some View {
+        NavigationStack{
+            VStack{
+                
+            }.navigationTitle("Add Contact")
+        }
+    }
+}
+
+#Preview {
+    AddContactView()
+}
---
+++ Contact App/ContactApp/ContactApp/ContactAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactAppApp.swift
+//  ContactApp
+//
+//  Created by Okan Aktas on 11.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct ContactAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}

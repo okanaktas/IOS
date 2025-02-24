ab55acd8cc5de6538e407dea7b5a6aee119c3a5f
Okan Aktaş <okanaktas02@gmail.com>
2025-02-24 20:53:01 +0300

    feature
    created model class and get api

---
+++ SevenApps/SevenApps/User.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct User: Codable {
+    let id: Int
+    let name: String
+    let email: String
+    let username: String
+} 
--- SevenApps/SevenApps/UserListViewController.swift
+++ SevenApps/SevenApps/UserListViewController.swift
@@ -2,15 +2,19 @@ import UIKit
 
 class UserListViewController: UIViewController {
     
+    private var users: [User] = []
+    
     private let tableView: UITableView = {
         let tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UserCell")
         return tableView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        fetchUsers()
     }
     
     private func setupUI() {
@@ -29,15 +33,49 @@ class UserListViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
     }
+    
+    private func fetchUsers() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else { return }
+        
+        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            guard let self = self,
+                  let data = data,
+                  error == nil else {
+                print("Error fetching users:", error?.localizedDescription ?? "Unknown error")
+                return
+            }
+            
+            do {
+                let users = try JSONDecoder().decode([User].self, from: data)
+                DispatchQueue.main.async {
+                    self.users = users
+                    self.tableView.reloadData()
+                }
+            } catch {
+                print("Error decoding users:", error)
+            }
+        }.resume()
+    }
 }
 
 extension UserListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return users.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath)
+        let user = users[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = user.name
+        content.secondaryText = user.email
+        cell.contentConfiguration = content
+        
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 } 
